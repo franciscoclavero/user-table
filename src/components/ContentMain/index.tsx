bb8625ed7012259fbd2ff1a40c786eaed97a89cf
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-01-18 11:53:23 -0300

    feat: create content main

---
+++ src/components/ContentMain/index.tsx
@@ -0,0 +1,11 @@
+import { styled } from "./style.module.css";
+
+const ContentMain = () => {
+  return (
+    <section>
+      Corpo do Projeto
+    </section>
+  );
+};
+
+export default ContentMain;
