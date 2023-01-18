9c82bf6f3d8fd6f3f74b174e00a79d94597e517b
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-01-18 16:18:51 -0300

    feat: create button new

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import styled from "./style.module.css";
+
+const Button = () => {
+  return (
+    <div> 
+      <img src="" alt="" />
+      Cadastrar
+    </div>
+  );
+};
+
+export default Button;
