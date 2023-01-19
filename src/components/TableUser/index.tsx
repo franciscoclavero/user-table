fd546308b0efead3e54123141a5c75999ab8f0bf
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-01-19 11:13:54 -0300

    feat: create axios consume in table user

--- src/components/TableUser/index.tsx
+++ src/components/TableUser/index.tsx
@@ -1,89 +1,51 @@
+import { useExportApi } from "hooks/useExternalApi";
+import { useEffect, useState } from "react";
 import styled from "./style.module.css";
 
+export type TypeUser = {
+  id: string,
+  name: string,
+  birthdate: string,
+  country: string,
+  city: string
+};
+
 const TableUser = () => {
+  const { data } = useExportApi<TypeUser[]>('');
+  const [ userList, setUserList ] = useState<TypeUser[]>([])
+
+  useEffect(() => {
+    setUserList( data );
+  }, [data]);
+
   return (
     <div className={styled.tableDiv}>
       <table className={styled.table}>
         <thead>
           <tr>
-            <th>Título 1</th>
-            <th>Título 2</th>
-            <th>Título 3</th>
-            <th>Título 4</th>
-            <th>Título 5</th>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Data de Aniversário</th>
+            <th>País</th>
+            <th>Cidade</th>
+            <th>Ações</th>
           </tr>
         </thead> 
         <tbody>
-          <tr>
-            <td>Valor Coluna 1</td>
-            <td>Valor Coluna 1</td>
-            <td>Valor Coluna 1</td>
-            <td>Valor Coluna 1</td>
-            <td>Valor Coluna 1</td>
-          </tr>
-          <tr>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-          </tr>
-          <tr>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-          </tr>
-          <tr>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-          </tr>
-          <tr>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-          </tr>
-          <tr>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-          </tr>
-          <tr>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-          </tr>
-          <tr>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-          </tr>
-          <tr>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-          </tr>
-          <tr>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-            <td>Valor Coluna 2</td>
-          </tr>
+          {
+            userList.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>{ item.id }</td>
+                  <td>{ item.name }</td>
+                  <td>{ item.birthdate }</td>
+                  <td>{ item.city }</td>
+                  <td>{ item.country }</td>
+                  <td> Ações </td>
+                </tr>
+              );
+            })
+          }
         </tbody>
       </table>
     </div>
