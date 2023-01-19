99638224413428a9138c49b37ab07aaab182fda0
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-01-19 10:33:43 -0300

    feat: create table component

--- src/components/TableUser/index.tsx
+++ src/components/TableUser/index.tsx
@@ -1,8 +1,92 @@
-import style from "./style.module.css";
+import styled from "./style.module.css";
 
 const TableUser = () => {
   return (
-    <div>Table</div>
+    <div className={styled.tableDiv}>
+      <table className={styled.table}>
+        <thead>
+          <tr>
+            <th>Título 1</th>
+            <th>Título 2</th>
+            <th>Título 3</th>
+            <th>Título 4</th>
+            <th>Título 5</th>
+          </tr>
+        </thead> 
+        <tbody>
+          <tr>
+            <td>Valor Coluna 1</td>
+            <td>Valor Coluna 1</td>
+            <td>Valor Coluna 1</td>
+            <td>Valor Coluna 1</td>
+            <td>Valor Coluna 1</td>
+          </tr>
+          <tr>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+          </tr>
+          <tr>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+          </tr>
+          <tr>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+          </tr>
+          <tr>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+          </tr>
+          <tr>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+          </tr>
+          <tr>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+          </tr>
+          <tr>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+          </tr>
+          <tr>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+          </tr>
+          <tr>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+            <td>Valor Coluna 2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
