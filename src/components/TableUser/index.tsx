f4e2540afbcdcbd8bfecf840a1091e173fed094d
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-01-20 11:37:25 -0300

    feat: create pagination in table user

--- src/components/TableUser/index.tsx
+++ src/components/TableUser/index.tsx
@@ -1,13 +1,32 @@
-import { TypeUser } from "@/pages/index";
 import { useAppStore } from "hooks/useAppStore";
-import { useExportApi } from "hooks/useExternalApi";
-import { useEffect, useState } from "react";
+import { MouseEvent, MouseEventHandler, useState } from "react";
 import Icon from "../Icon";
 
 import styled from "./style.module.css";
 
+interface InterfaceEvent {
+  target: MouseEvent<HTMLButtonElement, MouseEvent> | undefined;
+}
+
 const TableUser = () => {
   const { userList } = useAppStore(state => state.userList);
+
+  const [ itensPerPage, setItensPerPage ] = useState(8);
+  const [ currentPage, setCurrentPage ] = useState(0);
+
+  const pages = Math.ceil(userList.length / itensPerPage);
+  const startIndex = currentPage * itensPerPage;
+  const endIndex = startIndex + itensPerPage;
+  const currentItens = userList.slice(startIndex, endIndex);
+
+  const handleClickPagination = (page: number = 0, e: InterfaceEvent) => { 
+    let changePage = 0;
+
+    if (e.target) {
+      changePage = (e.target.value) ? Number(e.target.value) : (currentPage + page);
+    }
+    setCurrentPage(changePage);
+  };
 
   return (
     <div className={styled.tableDiv}>
@@ -24,7 +43,7 @@ const TableUser = () => {
         </thead> 
         <tbody>
           {
-            userList.map((item) => {
+            currentItens.map((item) => {
               const birthdate = new Date(item.birthdate);
               const formatBirthdate = `${birthdate.getDate()}/${birthdate.getMonth() + 1}/${birthdate.getFullYear()}`;
 
@@ -55,6 +74,20 @@ const TableUser = () => {
           }
         </tbody>
       </table>
+      <section className={styled.footer}>
+         <article className={styled.totalList}>
+          <p> Total de pessoas: {userList.length} </p>
+        </article>
+        <div className={styled.pagination}>
+          <button onClick={(event) => handleClickPagination(-1, event)}>Prev</button>
+          {
+            Array.from(Array(pages), (item, index) => {
+              return <button className={(index == currentPage) ? styled.active : ""}  key={index} value={index} onClick={(event) => handleClickPagination(index, event)}> {index + 1} </button>
+            })
+          }
+          <button onClick={(event) => handleClickPagination(1, event)}>Next</button>
+        </div>
+      </section>
     </div>
   );
 };
