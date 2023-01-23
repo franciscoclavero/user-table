93864de63fe58b02466d4130b23c39c38527cc85
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-01-23 10:23:27 -0300

    feat: create post and put in api

--- hooks/useExternalApi.tsx
+++ hooks/useExternalApi.tsx
@@ -1,6 +1,13 @@
 import { useEffect, useState } from "react";
 import axios, { AxiosRequestConfig } from "axios";
 
+interface FormDataType {
+  name: string, 
+  birthdate: string, 
+  country: string,
+  city: string,
+}
+
 export function useExportApi<T = unknown>(url: string, options?: AxiosRequestConfig) {
   const [ data, setData ] = useState<T | []>([]);
 
@@ -13,3 +20,21 @@ export function useExportApi<T = unknown>(url: string, options?: AxiosRequestCon
 
   return { data };
 };
+
+export const postData = (dataForm: FormDataType) => {
+  axios.post(`https://63c82502075b3f3a91db329c.mockapi.io/user`, {
+    name: dataForm.name,
+    birthdate: dataForm.birthdate,
+    country: dataForm.country,
+    city: dataForm.city
+  });
+};
+
+export const putData = (id: string, dataForm: FormDataType) => {
+  axios.post(`https://63c82502075b3f3a91db329c.mockapi.io/user/${id}`, {
+    name: dataForm.name,
+    birthdate: dataForm.birthdate,
+    country: dataForm.country,
+    city: dataForm.city
+  });
+}
